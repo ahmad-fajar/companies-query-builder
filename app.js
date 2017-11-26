281b694a12439d275270649ae0a9590c6fbf7de8
Ahmad Fajar <shin.fajar@gmail.com>
2017-11-26 09:45:17 +0700

    add atlas and update readme

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const Schema = require('./schema')
 const app = express()
 
 const port = 3000
-const mongoURL = 'mongodb://localhost:27017/glints'
+// const mongoURL = 'mongodb://localhost:27017/glints'
+const mongoURL = "mongodb://hacktiv8:hacktiv8Super@cluster0-shard-00-00-remkh.mongodb.net:27017,cluster0-shard-00-01-remkh.mongodb.net:27017,cluster0-shard-00-02-remkh.mongodb.net:27017/companies?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 
 mongoose.connect(mongoURL, (e) => {
   if (!e) console.log('>> Connected to database')
