02f1b8b861316d04db457b70ee81d10f82b823ed
Ahmad Fajar <shin.fajar@gmail.com>
2017-11-26 08:13:30 +0700

    company associations breakdown

--- functions.js
+++ functions.js
@@ -10,6 +10,34 @@ exports.getAllCompanies = () => {
 }
 
 
+exports.getAcquired = (name) => {
+  let query = {
+    name: name
+  }
+  return Companies.findOne(query)
+  .then(data => {
+    let send = []
+    data.acquisitions.map(acq => {
+      send.push({
+        name: acq.company.name
+      })
+    })
+    return send
+  })
+} // end of getAcquired
+
+
+exports.getAcquiring = name => {
+  let query = {
+    name: name
+  }
+  return Companies.findOne(query)
+  .then(data => {
+    return [{ name: data.acquisition.acquiring_company.name }]
+  })
+} // end of getAcquiring
+
+
 exports.getCompanyAssociations = name => {
   let query = {
     name: name
@@ -39,3 +67,21 @@ exports.getCompanyAssociations = name => {
   })
   .catch(e => console.log(e))
 } // end of getCompanyAssociation
+
+exports.getCompetitors = name => {
+  let query = {
+    name: name
+  }
+  return Companies.findOne(query)
+  .then(data => {
+    let send = []
+    data.competitions.map(comp => {
+      send.push({
+        name: comp.competitor.name,
+        relationship: 'competitor'
+      })
+    })
+    return send
+  })
+  .catch(e => console.log(e))
+} // end of getCompetitors
